8636a520bbd1f7e5b7214a8bbfd0eadadf201ea7
Joao Victor <jvpr.tech@gmail.com>
2023-07-15 23:13:32 -0300

    new component to represent button

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const props = defineProps({
+        buttonValue: {
+            type: String,
+            value: 'Submit'
+        },
+    })
+</script>
+
+<template>
+    <div class="input-container button">
+        <input type="button" :value="buttonValue">
+    </div>
+</template>
