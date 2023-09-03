81f7d3e686d4dd16d9cd6c3acadd02a1fa58eb70
Joao Victor <jvpr.tech@gmail.com>
2023-09-03 19:06:12 -0300

    updating alert component to use globally

---
+++ src/components/Alert.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { storeToRefs } from 'pinia'
+
+import { useAlertStore } from '@/stores/components/alert.js'
+
+const alertStore = useAlertStore()
+const { alert } = storeToRefs(alertStore)
+
+</script>
+
+
+<template>
+    <div v-if="alert" class="container">
+        <div class="alert alert-dismissable" :class="alert.type">
+            <buttton @click="alertStore.clear()" class="btn btn-link close">X</buttton>
+            {{ alert.message }}
+        </div>
+    </div>
+</template>
