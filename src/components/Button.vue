9206184a01893ac07a31266d3a056d51572a0d1b
Joao Victor <jvpr.tech@gmail.com>
2023-09-03 19:12:04 -0300

    updating button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,23 @@
 <script setup>
-	defineProps(['value'])
+	defineProps({
+        value: {
+            type: String,
+            default: ''
+        },
+        onClickFunction: {
+            type: Function,
+            default: () => {}
+        },
+        isSubmitting: {
+            type: Boolean,
+            default: false
+        }
+    })
 </script>
 
 <template>
     <div class="input-container button">
-        <input type="button" :value="value">
+        <input type="button" @click="onClickFunction" :value="value" :disabled="isSubmitting">
+        <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
     </div>
 </template>
