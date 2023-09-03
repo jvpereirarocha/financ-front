33dd7e5fdcfcc20331b0f178cba03ae8db43e4ef
Joao Victor <jvpr.tech@gmail.com>
2023-09-03 19:13:48 -0300

    updating the authStore changing login method and logout method

--- src/stores/auth/index.js
+++ src/stores/auth/index.js
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import { postRequest } from '@/services/api.js'
 import router from '@/router/index.js'
+import { useAlertStore } from '@/stores/components/alert.js'
 
 const API_AUTH_URL = `${import.meta.env.VITE_BASE_API_URL}/profile`
 
@@ -8,14 +9,30 @@ const API_AUTH_URL = `${import.meta.env.VITE_BASE_API_URL}/profile`
 export const useAuthStore = defineStore('auth', {
     state: () => ({
         user: JSON.parse(localStorage.getItem('user')) || null,
-        returnUrl: null,
+        returnUrl: {},
     }),
     actions: {
         async loginUser(email, password) {
-            const user = await postRequest(`${API_AUTH_URL}/login`, { email, password });
-            this.user = user;
-            localStorage.setItem('user', JSON.stringify(user));
-            router.push(this.returnUrl || '/');
+            const alertStore = useAlertStore();
+            try {
+                const request = await postRequest(`${API_AUTH_URL}/login`, { email, password });
+                if (!request.ok) {
+                    alertStore.error('Invalid credentials');
+                    return;
+                }
+                const data = await data.json();
+                this.user = data;
+                localStorage.setItem('user', JSON.stringify(this.user));
+                return this.user;
+            } catch(error) {
+                alertStore.error(error);
+                console.log(error)
+            }
         },
+        logout() {
+            this.user = null;
+            localStorage.removeItem('user');
+            router.push({ name: 'login' });
+        }
     }
 })
