1fb9e7263a75086eb1de289836cab9443425965f
Joao Victor <jvpr.tech@gmail.com>
2023-09-03 19:12:54 -0300

    remove router that wasn't be used

--- src/router/calcs/index.js
+++ src/router/calcs/index.js
@@ -4,11 +4,6 @@ const calcsRouter = [
         name: 'home',
         component: () => import('@/views/calcs/HomeView.vue')
     },
-    {
-        path: '/transactions',
-        name: 'listOfTransactions',
-        component: () => import('@/views/calc/ListTransactionsView.vue')
-    }
 ]
 
 export default calcsRouter
