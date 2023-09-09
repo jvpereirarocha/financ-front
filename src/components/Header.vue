3890c6d0cdff749e0cbae1a7d6c1a678c6b42970
Joao Victor <jvpr.tech@gmail.com>
2023-09-09 12:12:44 -0300

    creating new components

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="header--wrapper">
+        <div class="header--title">
+            <span>Bem-vindo(a)</span>
+            <h2>João Victor</h2>
+        </div>
+        <div class="user--info">
+            <div class="search-box">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="search" placeholder="Search" name="userInfo">
+            </div>
+            <img src="profile.png" alt="Perfil">
+        </div>
+    </header>
+</template>
