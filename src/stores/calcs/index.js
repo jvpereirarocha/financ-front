9d145bc943ea236ca7391adda70269984ec2510c
Joao Victor <jvpr.tech@gmail.com>
2023-11-10 13:46:10 -0300

    create new store to get resume info about balance, expenses and revenues

---
+++ src/stores/calcs/index.js
@@ -0,0 +1,42 @@
+import { defineStore } from 'pinia'
+import { postRequest, getRequest } from '@/services/api.js'
+import { useAuthStore } from '@/stores/auth'
+import { useAlertStore } from '@/stores/components/alert.js'
+
+const API_BALANCE_URL = `${import.meta.env.VITE_BASE_API_URL}/balances`
+
+
+export const useBalanceStore = defineStore('balance', {
+    state: () => ({
+        monthData: {},
+        yearData: {},
+    }),
+    actions: {
+        async getMonthData() {
+            const userStore = useAuthStore();
+            const alertStore = useAlertStore();
+            try {
+                const response = await getRequest(
+                    `${API_BALANCE_URL}/get_monthly_balance/`,
+                    { 'Authorization': userStore.token }
+                )
+                return response;
+            } catch (error) {
+                alertStore.error(error);
+            }
+        },
+        async getYearData() {
+            const userStore = useAuthStore();
+            const alertStore = useAlertStore();
+            try {
+                const response = await getRequest(
+                    `${API_BALANCE_URL}/get_yearly_balance/`,
+                    { 'Authorization': userStore.token }
+                )
+                return response;
+            } catch (error) {
+                alertStore.error(error);
+            }
+        },
+    }
+})
