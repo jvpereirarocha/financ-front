d9809021c7af56bd59189caab405b1ce6ba297ae
Joao Victor <jvpr.tech@gmail.com>
2024-01-11 08:14:37 -0300

    adding new routers

--- src/router/calcs/index.js
+++ src/router/calcs/index.js
@@ -4,6 +4,16 @@ const calcsRouter = [
         name: 'home',
         component: () => import('@/views/calcs/home/HomeView.vue')
     },
+    {
+        path: '/transactions',
+        name: 'transactions',
+        component: () => import('@/views/calcs/ListTransactionsView.vue')
+    },
+    {
+        path: '/reports',
+        name: 'reports',
+        component: () => import('@/views/calcs/ReportsView.vue')
+    },
 ]
 
 export default calcsRouter
